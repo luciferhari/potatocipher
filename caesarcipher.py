4b0190000f52158be41516d31466170374bb1af0
hackgoat <122145330+luciferhari@users.noreply.github.com>
2023-02-28 12:12:20 -0500

    Update caesarcipher.py

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,41 +1,53 @@
-def caesarcipher_encryption():
-    plaintext=input("enter the text=")
-    key=int(input("enter the key value="))
+def caesarcipher_encryption(plaintext,key):
+    
     encrypted_message=""
     for char in plaintext:
-        if char.isalpha:
+        if char.isalpha():
             code=ord(char.lower())+key
+            encrypted_text=chr(code)
             if code>=ord('z'):
                 code-=26
-            encrypted_text=chr(code)
-            if  char.isupper:
+            if  char.isupper():
                 encrypted_char=encrypted_text.upper()
                 encrypted_message+=encrypted_char 
+            else:
+                 encrypted_message+=encrypted_text
         else:
-            encrypted_message+=char
+                encrypted_message+=char
     return encrypted_message 
-def caesarcipher_decryption(plaintext,shift):
-    encrypted_message=""
-    for char in plaintext:
-        if char.isalpha:
+def caesarcipher_decryption(encrypted_text,shift):
+    decrypted_message=""
+    for char in encrypted_text:
+        if char.isalpha():
             code=ord(char.lower())-shift
+            decrypted_text=chr(code)
             if code>=ord('z'):
-                code-=26
-            encrypted_text=chr(code)
-            if  char.isupper:
-                encrypted_char=encrypted_text.upper()
-                encrypted_message+=encrypted_char 
+                code+=26
+            if  char.isupper():
+                decrypted_char=decrypted_text.upper()
+                decrypted_message+=decrypted_char
+            else:
+                 decrypted_message+=decrypted_text
         else:
-            encrypted_message+=char
-    return encrypted_message   
-
+                decrypted_message+=char
+    return decrypted_message   
 
 def switch(choice):
     if choice==1:
-        return caesarcipher_encryption()
-    if choice==2:
-        return caesarcipher_decryption()
+        plaintext=input("enter the text=")
+        key=int(input("enter the shift value="))
 
-choice=int(input("enter the choice:"))
+        return caesarcipher_encryption(plaintext,key)
+
+    elif choice==2:
+       
+         encrypted_text=input("enter the encrypted message:")
+         shift=int(input("enter the shift:"))
+
+         return caesarcipher_decryption(encrypted_text,shift)
+    else:
+        return "invaid choice"
+
+choice=int(input("enter the choice ,1 for encryption and 2 for decryption:"))
     
 print(switch(choice))
